7657baba396c140e20e583f2df5bd0204d149ee3
hiimyusheng <scott@cisyy.cc>
2024-04-18 10:40:00 +0800

    feat: 調整訊息內容

--- handler/gcp.go
+++ handler/gcp.go
@@ -23,12 +23,7 @@ func (g Gcp) HandleEvent(googleAlert model.Gcp, bot *linebot.Client, db mongo.Cl
 	tm := time.Unix(googleAlert.Incident.Started, 0)
 	loc, _ := time.LoadLocation("Asia/Taipei")
 	triggeredTime := tm.In(loc).Format("2006-01-02 15:04:05") + " (GMT+8)"
-	message := fmt.Sprintf(`⚠️ *%s* 快訊觸發！ ⚠️:
-	狀態： *%s*
-	時間： *%s*
-	內容： *%s*
-	請檢查系統運行狀況！`, googleAlert.Incident.PolicyName, googleAlert.Incident.State, triggeredTime, googleAlert.Incident.Summary)
-
+	message := fmt.Sprintf("⚠️ *%s* 快訊觸發！ ⚠️\r\n狀態： *%s*\r\n時間： *%s*\r\n內容： *%s*\r\n請檢查系統運行狀況！", googleAlert.Incident.PolicyName, googleAlert.Incident.State, triggeredTime, googleAlert.Incident.Summary)
 	for _, group := range groups {
 		if _, err := bot.PushMessage(group.GroupId, linebot.NewTextMessage(message)).Do(); err != nil {
 			utililty.Logger(3, err.Error())
